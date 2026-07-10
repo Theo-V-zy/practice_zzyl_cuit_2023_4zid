93b1be54b064c74b7dfc0f24216689f5b3665366
Theo-V-zy <315808745@qq.com>
2026-07-10 14:55:35 +0800

    按原型重构后台导航布局

---
+++ frontend/src/config/adminNavigation.js
@@ -0,0 +1,156 @@
+export const prototypeNavigation = [
+  {
+    id: 1,
+    mname: '工作台',
+    path: '/Dashboard',
+    sort: 1,
+    subItems: [{ id: 2, mname: '工作台', path: '/Dashboard', sort: 1, subItems: [] }]
+  },
+  {
+    id: 10,
+    mname: '来访管理',
+    sort: 2,
+    subItems: [
+      {
+        id: 11,
+        mname: '预约来访',
+        sort: 1,
+        subItems: [
+          { id: 12, mname: '预约登记', path: '/Visit', sort: 1, subItems: [] },
+          { id: 13, mname: '来访登记', path: '/VisitRecord', sort: 2, subItems: [] }
+        ]
+      }
+    ]
+  },
+  {
+    id: 20,
+    mname: '入退管理',
+    sort: 3,
+    subItems: [
+      { id: 21, mname: '入住管理', sort: 1, subItems: [{ id: 22, mname: '入住办理', path: '/Apply', sort: 1, subItems: [] }] },
+      { id: 23, mname: '退住管理', sort: 2, subItems: [{ id: 24, mname: '退住办理', path: '/Checkout', sort: 1, subItems: [] }] }
+    ]
+  },
+  {
+    id: 30,
+    mname: '在住管理',
+    sort: 4,
+    subItems: [
+      { id: 31, mname: '合同管理', sort: 1, subItems: [{ id: 32, mname: '合同跟踪', path: '/Resident', sort: 1, subItems: [] }] },
+      {
+        id: 33,
+        mname: '床位管理',
+        sort: 2,
+        subItems: [
+          { id: 34, mname: '床位房型', path: '/Bed', sort: 1, subItems: [] },
+          { id: 35, mname: '智能床位', path: '/SmartBed', sort: 2, subItems: [] },
+          { id: 36, mname: '房型设置', path: '/RoomType', sort: 3, subItems: [] }
+        ]
+      },
+      { id: 37, mname: '请假管理', sort: 3, subItems: [{ id: 38, mname: '请假管理', path: '/Leave', sort: 1, subItems: [] }] }
+    ]
+  },
+  {
+    id: 40,
+    mname: '服务管理',
+    sort: 5,
+    subItems: [
+      {
+        id: 41,
+        mname: '护理计划',
+        sort: 1,
+        subItems: [
+          { id: 42, mname: '护理等级', path: '/NursingLevel', sort: 1, subItems: [] },
+          { id: 43, mname: '护理计划', path: '/NursingPlain', sort: 2, subItems: [] },
+          { id: 44, mname: '护理项目', path: '/NursingItem', sort: 3, subItems: [] }
+        ]
+      },
+      {
+        id: 45,
+        mname: '护理任务',
+        sort: 2,
+        subItems: [
+          { id: 46, mname: '负责老人', path: '/ElderAssignment', sort: 1, subItems: [] },
+          { id: 47, mname: '任务安排', path: '/NursingTask', sort: 2, subItems: [] }
+        ]
+      }
+    ]
+  },
+  {
+    id: 50,
+    mname: '订单管理',
+    sort: 6,
+    subItems: [
+      { id: 51, mname: '订单管理', path: '/Order', sort: 1, subItems: [] },
+      { id: 52, mname: '退款管理', path: '/Refund', sort: 2, subItems: [] }
+    ]
+  },
+  {
+    id: 60,
+    mname: '财务管理',
+    sort: 7,
+    subItems: [
+      { id: 61, mname: '账单管理', sort: 1, subItems: [{ id: 62, mname: '入账列表', path: '/Bill', sort: 1, subItems: [] }, { id: 63, mname: '欠费老人', path: '/Arrears', sort: 2, subItems: [] }] },
+      { id: 64, mname: '预存管理', sort: 2, subItems: [{ id: 65, mname: '预缴款充值', path: '/Prepay', sort: 1, subItems: [] }, { id: 66, mname: '余额查询', path: '/Balance', sort: 2, subItems: [] }] }
+    ]
+  },
+  {
+    id: 70,
+    mname: '客户管理',
+    sort: 8,
+    subItems: [{ id: 71, mname: '客户信息', path: '/Customer', sort: 1, subItems: [] }]
+  },
+  {
+    id: 80,
+    mname: '权限管理',
+    sort: 9,
+    subItems: [
+      { id: 81, mname: '用户管理', sort: 1, subItems: [{ id: 82, mname: '用户信息', path: '/UserManage', sort: 1, subItems: [] }] },
+      {
+        id: 83,
+        mname: '权限配置',
+        sort: 2,
+        subItems: [
+          { id: 84, mname: '角色管理-菜单权限', path: '/RoleManage', sort: 1, subItems: [] },
+          { id: 85, mname: '角色管理-数据权限', path: '/RoleDataScope', sort: 2, subItems: [] },
+          { id: 86, mname: '菜单管理', path: '/MenuManage', sort: 3, subItems: [] },
+          { id: 87, mname: '部门管理', path: '/Department', sort: 4, subItems: [] },
+          { id: 88, mname: '职位管理', path: '/Post', sort: 5, subItems: [] }
+        ]
+      }
+    ]
+  },
+  {
+    id: 90,
+    mname: '协同工作',
+    sort: 10,
+    subItems: [
+      { id: 91, mname: '我的待办', path: '/Todo', sort: 1, subItems: [] },
+      { id: 92, mname: '我的申请', path: '/Application', sort: 2, subItems: [] }
+    ]
+  },
+  {
+    id: 100,
+    mname: '智能监测',
+    sort: 11,
+    subItems: [
+      { id: 101, mname: '设备管理', sort: 1, subItems: [{ id: 102, mname: '设备管理', path: '/Device', sort: 1, subItems: [] }] },
+      { id: 103, mname: '报警管理', sort: 2, subItems: [{ id: 104, mname: '报警数据', path: '/Alert', sort: 1, subItems: [] }, { id: 105, mname: '报警规则', path: '/AlertRule', sort: 2, subItems: [] }] }
+    ]
+  },
+  {
+    id: 200,
+    mname: '个人中心',
+    topVisible: false,
+    subItems: [
+      { id: 201, mname: '个人信息', path: '/UserInfo', sort: 1, subItems: [] },
+      { id: 202, mname: '修改密码', path: '/ModifyPwd', sort: 2, subItems: [] }
+    ]
+  },
+  {
+    id: 210,
+    mname: '消息中心',
+    topVisible: false,
+    subItems: [{ id: 211, mname: '消息通知', path: '/Messages', sort: 1, subItems: [] }]
+  }
+]
--- frontend/src/layouts/AdminLayout.vue
+++ frontend/src/layouts/AdminLayout.vue
@@ -1,161 +1,162 @@
 <template>
-  <el-container class="admin-shell">
-    <el-aside :width="isCollapse ? '64px' : '220px'" class="admin-sidebar">
-      <div class="brand-block">
-        <div class="brand-mark">养</div>
-        <span v-show="!isCollapse" class="brand-name">智慧养老服务平台</span>
+  <div class="admin-shell">
+    <header class="admin-header">
+      <button class="brand-block" type="button" @click="router.push('/Dashboard')">
+        <img class="brand-logo" :src="logoMark" alt="中州养老" />
+        <span class="brand-copy">
+          <strong>中州养老</strong>
+          <small>ZHONG ZHOU YANG LAO</small>
+        </span>
+      </button>
+
+      <div class="topbar">
+        <nav class="top-navigation" aria-label="功能模块">
+          <button
+            v-for="module in topModules"
+            :key="module.id"
+            class="top-module"
+            :class="{ 'is-active': activeModule?.id === module.id }"
+            type="button"
+            @click="selectTopModule(module)"
+          >
+            {{ module.mname }}
+          </button>
+        </nav>
+
+        <el-dropdown trigger="click" @command="handleUserCommand">
+          <button class="user-trigger" type="button">
+            <img class="user-avatar" :src="userImage || defaultAvatar" alt="管理员头像" />
+            <span>{{ realName || '管理员' }}</span>
+            <el-icon :size="13"><ArrowDown /></el-icon>
+          </button>
+          <template #dropdown>
+            <el-dropdown-menu>
+              <el-dropdown-item command="profile">个人中心</el-dropdown-item>
+              <el-dropdown-item command="password">修改密码</el-dropdown-item>
+              <el-dropdown-item command="messages">消息通知</el-dropdown-item>
+              <el-dropdown-item divided command="logout">退出登录</el-dropdown-item>
+            </el-dropdown-menu>
+          </template>
+        </el-dropdown>
       </div>
+    </header>
 
-      <el-scrollbar class="menu-scrollbar">
-        <el-menu
-          :default-active="route.path"
-          :collapse="isCollapse"
-          :collapse-transition="false"
-          :unique-opened="true"
-          class="admin-menu"
-          router
-        >
-          <template v-for="menu in menuList" :key="menu.id">
-            <el-menu-item v-if="!hasChildren(menu) && menu.path" :index="menu.path">
-              <el-icon><component :is="iconFor(menu.mname)" /></el-icon>
-              <template #title>{{ menu.mname }}</template>
-            </el-menu-item>
-
-            <el-sub-menu v-else-if="hasChildren(menu)" :index="String(menu.id)">
-              <template #title>
+    <div class="admin-workspace">
+      <aside class="admin-sidebar">
+        <el-scrollbar class="menu-scrollbar">
+          <el-menu
+            :default-active="route.path"
+            :default-openeds="defaultOpeneds"
+            class="admin-menu"
+            router
+          >
+            <template v-for="menu in sideMenuItems" :key="menu.id">
+              <el-menu-item v-if="!hasChildren(menu) && menu.path" :index="menu.path">
                 <el-icon><component :is="iconFor(menu.mname)" /></el-icon>
                 <span>{{ menu.mname }}</span>
-              </template>
-              <template v-for="subMenu in childrenOf(menu)" :key="subMenu.id">
-                <el-menu-item
-                  v-if="!hasChildren(subMenu) && subMenu.path"
-                  :index="subMenu.path"
-                >
-                  {{ subMenu.mname }}
-                </el-menu-item>
-                <el-sub-menu v-else-if="hasChildren(subMenu)" :index="String(subMenu.id)">
-                  <template #title>{{ subMenu.mname }}</template>
-                  <el-menu-item
-                    v-for="leaf in childrenOf(subMenu)"
-                    :key="leaf.id"
-                    :index="leaf.path"
-                  >
-                    {{ leaf.mname }}
+              </el-menu-item>
+
+              <el-sub-menu v-else-if="hasChildren(menu)" :index="String(menu.id)">
+                <template #title>
+                  <el-icon><component :is="iconFor(menu.mname)" /></el-icon>
+                  <span>{{ menu.mname }}</span>
+                </template>
+                <template v-for="subMenu in childrenOf(menu)" :key="subMenu.id">
+                  <el-menu-item v-if="!hasChildren(subMenu) && subMenu.path" :index="subMenu.path">
+                    {{ subMenu.mname }}
                   </el-menu-item>
-                </el-sub-menu>
-              </template>
-            </el-sub-menu>
-          </template>
-        </el-menu>
-      </el-scrollbar>
-    </el-aside>
-
-    <el-container class="admin-main-container">
-      <el-header class="admin-header">
-        <div class="header-left">
-          <el-button
-            class="collapse-button"
-            link
-            :aria-label="isCollapse ? '展开菜单' : '收起菜单'"
-            @click="isCollapse = !isCollapse"
-          >
-            <el-icon :size="20"><Expand v-if="isCollapse" /><Fold v-else /></el-icon>
-          </el-button>
-          <el-breadcrumb separator="/">
-            <el-breadcrumb-item>工作台</el-breadcrumb-item>
-            <el-breadcrumb-item v-if="currentTitle && currentTitle !== '工作台'">{{ currentTitle }}</el-breadcrumb-item>
-          </el-breadcrumb>
-        </div>
-
-        <div class="header-right">
-          <el-tooltip content="消息中心" placement="bottom">
-            <el-badge :hidden="unreadCount === 0" :value="unreadCount" :max="99">
-              <el-button link class="header-icon-button" @click="goMessages">
-                <el-icon :size="19"><Bell /></el-icon>
-              </el-button>
-            </el-badge>
-          </el-tooltip>
-
-          <el-dropdown trigger="click" @command="handleUserCommand">
-            <button class="user-trigger" type="button">
-              <el-avatar :size="30" :src="userImage">{{ userInitial }}</el-avatar>
-              <span class="user-name">{{ realName || '管理员' }}</span>
-              <el-icon><ArrowDown /></el-icon>
-            </button>
-            <template #dropdown>
-              <el-dropdown-menu>
-                <el-dropdown-item command="profile">个人中心</el-dropdown-item>
-                <el-dropdown-item command="password">修改密码</el-dropdown-item>
-                <el-dropdown-item divided command="logout">退出登录</el-dropdown-item>
-              </el-dropdown-menu>
+                  <el-sub-menu v-else-if="hasChildren(subMenu)" :index="String(subMenu.id)">
+                    <template #title>{{ subMenu.mname }}</template>
+                    <el-menu-item
+                      v-for="leaf in childrenOf(subMenu)"
+                      :key="leaf.id"
+                      :index="leaf.path"
+                    >
+                      {{ leaf.mname }}
+                    </el-menu-item>
+                  </el-sub-menu>
+                </template>
+              </el-sub-menu>
             </template>
-          </el-dropdown>
-        </div>
-      </el-header>
+          </el-menu>
+        </el-scrollbar>
+        <p class="copyright">Copyright @ 2019-2020 Tencent.<br />All Rights Reserved</p>
+      </aside>
 
-      <el-main class="admin-content">
+      <main class="admin-content">
         <div class="content-inner">
           <slot />
         </div>
-      </el-main>
-    </el-container>
-  </el-container>
+      </main>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { computed, onMounted, ref } from 'vue'
+import { computed, onMounted, ref, watch } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import axios from 'axios'
 import {
   ArrowDown,
   Bell,
-  Expand,
-  Fold,
+  Calendar,
+  Checked,
+  CreditCard,
+  DataAnalysis,
+  House,
   Menu as MenuIcon,
+  Monitor,
+  OfficeBuilding,
+  Service,
   Setting,
   User,
-  Odometer,
-  Service,
-  Checked,
-  Monitor,
-  CreditCard,
   UserFilled
 } from '@element-plus/icons-vue'
+import { prototypeNavigation } from '@/config/adminNavigation'
+import logoMark from '@/assets/zhyl-logo-mark.png'
+import defaultAvatar from '@/assets/zhyl-user-avatar.png'
 
 const route = useRoute()
 const router = useRouter()
-const isCollapse = ref(false)
 const realName = ref('')
 const userImage = ref('')
-const unreadCount = ref(0)
-const menuList = ref([])
+const menuList = ref(prototypeNavigation)
+const activeModuleId = ref(1)
 
-const fallbackMenus = [
-  { id: 1, mname: '工作台', path: '/Dashboard', subItems: [] },
-  { id: 2, mname: '个人中心', path: '', subItems: [
-    { id: 21, mname: '个人信息', path: '/UserInfo', subItems: [] },
-    { id: 22, mname: '修改密码', path: '/ModifyPwd', subItems: [] }
-  ] },
-  { id: 3, mname: '服务管理', path: '', subItems: [
-    { id: 31, mname: '护理项目', path: '/NursingItem', subItems: [] },
-    { id: 32, mname: '护理计划', path: '/NursingPlain', subItems: [] }
-  ] }
-]
+const expectedTopNames = ['工作台', '来访管理', '入退管理', '在住管理', '服务管理', '订单管理', '财务管理', '客户管理', '权限管理', '协同工作', '智能监测']
 
 const iconMap = {
-  工作台: Odometer,
-  个人中心: User,
-  服务管理: Service,
+  工作台: House,
+  预约来访: Calendar,
+  入住管理: OfficeBuilding,
+  退住管理: OfficeBuilding,
+  合同管理: CreditCard,
+  床位管理: OfficeBuilding,
+  请假管理: Calendar,
+  护理计划: Service,
+  护理任务: Checked,
   订单管理: CreditCard,
-  财务管理: CreditCard,
+  账单管理: CreditCard,
+  预存管理: CreditCard,
+  客户信息: UserFilled,
+  用户管理: User,
   权限配置: Setting,
-  智能监测: Monitor,
-  协同工作: Checked,
-  客户管理: UserFilled
+  我的待办: Checked,
+  我的申请: DataAnalysis,
+  设备管理: Monitor,
+  报警管理: Bell,
+  个人中心: User,
+  消息中心: Bell
 }
 
-const currentTitle = computed(() => route.meta?.title || '')
-const userInitial = computed(() => (realName.value || '管').slice(0, 1))
+const topModules = computed(() => menuList.value.filter((item) => item.topVisible !== false))
+const activeModule = computed(() => menuList.value.find((item) => item.id === activeModuleId.value) || topModules.value[0])
+const sideMenuItems = computed(() => {
+  const module = activeModule.value
+  if (!module) return []
+  return hasChildren(module) ? childrenOf(module) : [module]
+})
+const defaultOpeneds = computed(() => collectParentIds(sideMenuItems.value))
 
 function childrenOf(menu) {
   return Array.isArray(menu?.subItems) ? menu.subItems : []
@@ -169,38 +170,79 @@ function iconFor(name) {
   return iconMap[name] || MenuIcon
 }
 
+function sortTree(items) {
+  return [...items]
+    .sort((a, b) => (a.sort || 0) - (b.sort || 0))
+    .map((item) => ({ ...item, subItems: sortTree(childrenOf(item)) }))
+}
+
+function collectParentIds(items) {
+  return items.flatMap((item) => hasChildren(item) ? [String(item.id), ...collectParentIds(childrenOf(item))] : [])
+}
+
+function containsPath(item, path) {
+  return item.path === path || childrenOf(item).some((child) => containsPath(child, path))
+}
+
+function firstPath(item) {
+  if (item.path) return item.path
+  for (const child of childrenOf(item)) {
+    const path = firstPath(child)
+    if (path) return path
+  }
+  return '/Dashboard'
+}
+
+function syncModuleWithRoute(path) {
+  const matched = menuList.value.find((module) => containsPath(module, path))
+  if (matched) activeModuleId.value = matched.id
+}
+
+function selectTopModule(module) {
+  activeModuleId.value = module.id
+  router.push(firstPath(module))
+}
+
+function useServerMenus(data) {
+  if (!Array.isArray(data)) return false
+  const names = data.map((item) => item.mname)
+  return expectedTopNames.every((name) => names.includes(name))
+}
+
 function loadMenus() {
   axios.get('/sysMenus')
     .then(({ data }) => {
-      menuList.value = Array.isArray(data) && data.length ? data : fallbackMenus
+      if (useServerMenus(data)) {
+        const visibleTop = sortTree(data.filter((item) => expectedTopNames.includes(item.mname)))
+        const utilityMenus = prototypeNavigation.filter((item) => item.topVisible === false)
+        menuList.value = [...visibleTop, ...utilityMenus]
+      }
+      syncModuleWithRoute(route.path)
     })
-    .catch(() => {
-      menuList.value = fallbackMenus
-    })
+    .catch(() => {})
 }
 
 function loadUserInfo() {
   axios.get('/loadInfo')
     .then(({ data }) => {
-      realName.value = data?.uname || ''
+      realName.value = data?.uname || data?.realname || ''
       userImage.value = data?.image || ''
     })
     .catch(() => {})
 }
 
-function goMessages() {
-  router.push('/Messages')
-}
-
 function handleUserCommand(command) {
   if (command === 'profile') router.push('/UserInfo')
   if (command === 'password') router.push('/ModifyPwd')
+  if (command === 'messages') router.push('/Messages')
   if (command === 'logout') {
     localStorage.clear()
     sessionStorage.clear()
     router.replace('/')
   }
 }
+
+watch(() => route.path, syncModuleWithRoute, { immediate: true })
 
 onMounted(() => {
   loadMenus()
@@ -209,117 +251,245 @@ onMounted(() => {
 </script>
 
 <style scoped>
-.admin-shell,
-.admin-main-container {
+.admin-shell {
+  display: grid;
+  grid-template-rows: 64px minmax(0, 1fr);
   width: 100%;
   height: 100%;
   overflow: hidden;
+  color: #333;
+  background: #f4f5f7;
 }
 
-.admin-sidebar {
-  flex: 0 0 auto;
-  background: #1f2d3d;
-  transition: width 0.2s ease;
+.admin-header {
+  display: grid;
+  grid-template-columns: 232px minmax(0, 1fr);
+  height: 64px;
+  background: #f6f7f9;
+  border-bottom: 1px solid #e7e9ed;
 }
 
 .brand-block {
   display: flex;
   align-items: center;
   height: 64px;
-  padding: 0 16px;
-  color: #fff;
-  white-space: nowrap;
-  overflow: hidden;
-  border-bottom: 1px solid rgba(255, 255, 255, 0.08);
-}
-
-.brand-mark {
-  display: grid;
-  place-items: center;
-  flex: 0 0 32px;
-  width: 32px;
-  height: 32px;
-  border-radius: 4px;
-  background: #2f8f83;
-  font-size: 18px;
-  font-weight: 600;
-}
-
-.brand-name {
-  margin-left: 10px;
-  font-size: 15px;
-  font-weight: 600;
-}
-
-.menu-scrollbar {
-  height: calc(100% - 64px);
-}
-
-.admin-menu {
-  border-right: 0;
-  background: transparent;
-}
-
-.admin-menu:not(.el-menu--collapse) {
-  width: 220px;
-}
-
-.admin-header {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  height: 64px;
-  padding: 0 24px;
+  padding: 4px 18px 4px 20px;
+  color: #333;
   background: #fff;
-  border-bottom: 1px solid #ebeef5;
-}
-
-.header-left,
-.header-right,
-.user-trigger {
-  display: flex;
-  align-items: center;
-}
-
-.header-left {
-  gap: 14px;
-}
-
-.header-right {
-  gap: 20px;
-}
-
-.collapse-button,
-.header-icon-button {
-  color: #606266;
-}
-
-.user-trigger {
-  gap: 8px;
-  padding: 0;
-  color: #303133;
-  background: transparent;
   border: 0;
+  border-right: 1px solid #e7e9ed;
   cursor: pointer;
 }
 
-.user-name {
-  max-width: 120px;
-  overflow: hidden;
-  text-overflow: ellipsis;
+.brand-logo {
+  flex: 0 0 48px;
+  width: 48px;
+  height: 48px;
+  object-fit: contain;
+}
+
+.brand-copy {
+  display: flex;
+  min-width: 0;
+  margin-left: 8px;
+  flex-direction: column;
+  align-items: flex-start;
   white-space: nowrap;
 }
 
+.brand-copy strong {
+  font-family: STXingkai, "Xingkai SC", "Kaiti SC", serif;
+  font-size: 25px;
+  line-height: 30px;
+}
+
+.brand-copy small {
+  font-family: Arial, sans-serif;
+  font-size: 9px;
+  line-height: 14px;
+  color: rgba(51, 51, 51, 0.8);
+}
+
+.topbar {
+  display: flex;
+  min-width: 0;
+  align-items: stretch;
+}
+
+.top-navigation {
+  display: flex;
+  min-width: 0;
+  flex: 1;
+  align-items: stretch;
+  gap: 14px;
+  padding: 0 20px;
+  overflow-x: auto;
+  overflow-y: hidden;
+  scrollbar-width: none;
+}
+
+.top-navigation::-webkit-scrollbar {
+  display: none;
+}
+
+.top-module {
+  position: relative;
+  flex: 0 0 82px;
+  height: 64px;
+  padding: 0;
+  color: rgba(0, 0, 0, 0.6);
+  background: transparent;
+  border: 0;
+  font-size: 14px;
+  cursor: pointer;
+  white-space: nowrap;
+}
+
+.top-module::after {
+  position: absolute;
+  right: 18px;
+  bottom: 0;
+  left: 18px;
+  height: 2px;
+  content: '';
+  background: transparent;
+}
+
+.top-module:hover,
+.top-module.is-active {
+  color: #0052d9;
+}
+
+.top-module.is-active::after {
+  background: #0052d9;
+}
+
+.user-trigger {
+  display: flex;
+  flex: 0 0 auto;
+  min-width: 126px;
+  height: 64px;
+  padding: 0 18px 0 10px;
+  align-items: center;
+  gap: 8px;
+  color: #333;
+  background: transparent;
+  border: 0;
+  cursor: pointer;
+  font-size: 14px;
+}
+
+.user-avatar {
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+
+.admin-workspace {
+  display: grid;
+  grid-template-columns: 232px minmax(0, 1fr);
+  min-height: 0;
+}
+
+.admin-sidebar {
+  display: grid;
+  grid-template-rows: minmax(0, 1fr) auto;
+  min-height: 0;
+  background: #fff;
+  border-right: 1px solid #e7e9ed;
+}
+
+.menu-scrollbar {
+  min-height: 0;
+}
+
+.admin-menu {
+  padding: 14px 8px;
+  border-right: 0;
+}
+
+.admin-menu :deep(.el-menu-item),
+.admin-menu :deep(.el-sub-menu__title) {
+  height: 40px;
+  margin: 0;
+  color: rgba(0, 0, 0, 0.6);
+  border-radius: 0;
+  font-size: 14px;
+  line-height: 40px;
+}
+
+.admin-menu :deep(.el-sub-menu__title:hover),
+.admin-menu :deep(.el-menu-item:hover) {
+  color: rgba(0, 0, 0, 0.9);
+  background: #f5f7fa;
+}
+
+.admin-menu :deep(.el-menu-item.is-active) {
+  color: #0052d9;
+  background: #eef4ff;
+}
+
+.admin-menu :deep(.el-icon) {
+  font-size: 16px;
+}
+
+.copyright {
+  margin: 12px 16px 18px;
+  color: rgba(0, 0, 0, 0.35);
+  font-size: 11px;
+  line-height: 18px;
+  text-align: center;
+}
+
 .admin-content {
-  padding: 20px;
+  min-width: 0;
+  min-height: 0;
+  padding: 24px;
   overflow: auto;
-  background: #f4f6f8;
+  background: #f4f5f7;
 }
 
 .content-inner {
+  min-width: 0;
   min-height: 100%;
-  padding: 20px;
+  padding: 24px;
   background: #fff;
-  border: 1px solid #ebeef5;
+  border: 1px solid #e7e9ed;
+}
+
+@media (max-width: 1200px) {
+  .admin-header,
+  .admin-workspace {
+    grid-template-columns: 210px minmax(0, 1fr);
+  }
+
+  .brand-block {
+    padding-right: 10px;
+    padding-left: 14px;
+  }
+
+  .brand-logo {
+    width: 44px;
+    height: 44px;
+    flex-basis: 44px;
+  }
+
+  .brand-copy strong {
+    font-size: 22px;
+  }
+
+  .top-navigation {
+    gap: 4px;
+    padding: 0 10px;
+  }
+
+  .top-module {
+    flex-basis: 76px;
+  }
+
+  .admin-content {
+    padding: 18px;
+  }
 }
 </style>
